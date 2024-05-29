bb3fa060cae930c182e9e2bd8170e5a9d659fdc7
Tomas Santana <tomas.30604530@uru.edu>
2024-05-29 02:49:18 -0400

    Logout implementado

--- src/apiRoutes.ts
+++ src/apiRoutes.ts
@@ -2,6 +2,7 @@ const host = "https://127.0.0.1:3000";
 export default {
   login: `${host}/auth/login`,
   register: `${host}/auth/register`,
+  logout: `${host}/auth/logout`,
   authorized: `${host}/auth`,
   createPasswordReset: `${host}/auth/create-password-reset`,
   passwordReset: `${host}/auth/reset-password`,
--- src/views/ProtectedView.vue
+++ src/views/ProtectedView.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
-    import {
-        Card, 
-        CardContent,
-        CardTitle,
-        CardDescription,
-        CardHeader
-    } from '@/components/ui/card'
-    import { Button } from '@/components/ui/button';
-    import { toast } from 'vue-sonner';
+import {
+    Card,
+    CardContent,
+    CardTitle,
+    CardDescription,
+    CardHeader
+} from '@/components/ui/card'
+import { Button } from '@/components/ui/button';
+import { toast } from 'vue-sonner';
+import apiRoutes from '@/apiRoutes';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const handleLogout = async () => {
+    const response = await fetch(apiRoutes.logout, {
+        method: 'GET',
+        credentials: 'include'
+    })
+    if (response.ok) {
+        router.push('/login')
+    } else {
+        toast.error('Error al cerrar sesión')
+    }
+}
 </script>
 
 <template>
@@ -20,12 +35,14 @@
                 <CardDescription>
                     Esta es una vista protegida
                 </CardDescription>
-                <CardContent class="flex mt-4">
-                   <Button 
-                   @click="toast.success('Wuuuuuu!')"
-                   class="flex-1">
+                <CardContent class="flex mt-4 flex-col gap-2">
+                    <Button @click="toast.success('Wuuuuuu!')" class="flex-1">
                         A celebrar
-                   </Button>
+                    </Button>
+                    <Button @click="handleLogout" variant="destructive"class="flex-1">
+                        Cerrar sesión
+                    </Button>
+
                 </CardContent>
             </CardHeader>
         </Card>
